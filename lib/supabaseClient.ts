f92e693269b8dacb345d557c6a6b69ebefa8f9ad
Mehmet Catalsakal <info@opny.ai>
2025-06-22 21:53:07 +0200

    Log ENV vars for debugging

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,5 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
+// TEST: ENV-Variablen im Build ausgeben
+console.log(
+  'Vercel ENV:',
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
+
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
