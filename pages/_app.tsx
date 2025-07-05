9169a5af060b66846ea431f1ae61baf9b25528e8
OpnyAI <mehmet_ctskl@hotmail.de>
2025-07-05 10:22:14 +0200

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,25 @@
+import Head from "next/head";
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Favicon für alle Browser */}
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        {/* Apple Touch Icon */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        {/* Android Chrome */}
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+        {/* Manifest für PWA */}
+        <link rel="manifest" href="/site.webmanifest" />
+        {/* Theme Color */}
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
